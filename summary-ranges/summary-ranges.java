623211c17b7b9d92a368a915efd9ad15467ef0a3
DmitrySwan <70700631+DmitrySwan@users.noreply.github.com>
2021-08-22 11:19:16 +0400

    Time: 6 ms (70.23%), Space: 37.8 MB (25.25%) - LeetHub

--- summary-ranges/summary-ranges.java
+++ summary-ranges/summary-ranges.java
@@ -1,41 +1,21 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        List<String> solution = new LinkedList<>();
-        if(nums.length==0) return solution;
-
-        StringBuilder buffer = new StringBuilder(String.valueOf(nums[0]));
-        if(nums.length==1) {
-            solution.add(buffer.toString());
-            return solution;
+        List<String> list= new ArrayList<>();
+        if(nums.length==1){
+            list.add(nums[0]+"");
+            return list;
         }
-        int count=0;
-        for (int i=1; i<nums.length ; i++) {
-            if (nums[i] == nums[i-1] + 1) {
-                count ++;
-                if (nums.length!=i+1) {
-                    continue;
-                }
+        for(int i=0;i<nums.length;i++){
+            int a=nums[i];
+            while(i+1<nums.length&&(nums[i+1]-nums[i])==1){
+                i++;
             }
-            else if(count > 0){
-                buffer.append("->").append(nums[i - 1]);
-                solution.add(buffer.toString());
-                count = 0;
-                buffer = new StringBuilder(String.valueOf(nums[i]));
-
-            }
-            else {
-                solution.add(buffer.toString());
-                buffer = new StringBuilder(String.valueOf(nums[i]));
-
-            }
-            if(nums.length == i+1) {
-                if (count>0){
-                    buffer.append("->").append(nums[i]);
-                }
-                solution.add(buffer.toString());
+            if(a!=nums[i]){
+                list.add(a+"->"+nums[i]);
+            }else{
+                list.add(a+"");
             }
         }
-        System.out.println("test string for github");
-        return solution;
+        return list;
     }
 }
