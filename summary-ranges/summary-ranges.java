fb2bb2342baff758989f5b6dc07e85293e92a34c
DmitrySwan <70700631+DmitrySwan@users.noreply.github.com>
2021-08-22 11:23:06 +0400

    Time: 0 ms (100.00%), Space: 36.7 MB (99.10%) - LeetHub

--- summary-ranges/summary-ranges.java
+++ summary-ranges/summary-ranges.java
@@ -1,21 +1,27 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        List<String> list= new ArrayList<>();
-        if(nums.length==1){
-            list.add(nums[0]+"");
-            return list;
-        }
-        for(int i=0;i<nums.length;i++){
-            int a=nums[i];
-            while(i+1<nums.length&&(nums[i+1]-nums[i])==1){
-                i++;
+        List<String> sortedRangeList = new ArrayList<>();
+
+        StringBuilder range = new StringBuilder();
+
+        for(int index = 0; index < nums.length;) {
+
+            range.append(nums[index]);
+
+            int startIndex = ++index;
+
+            while(index < nums.length && nums[index] == nums[index - 1] + 1){
+                ++index;
             }
-            if(a!=nums[i]){
-                list.add(a+"->"+nums[i]);
-            }else{
-                list.add(a+"");
+
+            if(startIndex != index){
+                range.append("->").append(nums[index - 1]);
             }
+
+            sortedRangeList.add(range.toString());
+            range.setLength(0);
         }
-        return list;
+
+        return sortedRangeList;
     }
 }
